9f115b24cad9d3458fc490819553b172aa991201
Colleen <crusting@horizon.csueastbay.edu>
2026-04-14 16:00:28 -0700

    Constructor, Add, Remove, Contains, Operator, Rehash

--- MapT.cpp
+++ MapT.cpp
@@ -11,17 +11,43 @@ const double DEFAULT_LOAD = 1.0;
 
 template<class K, class T>
 MapT<K, T>::MapT() {
+    buckets = new forward_list<K, T>[DEFAULT_BUCKETS];
 
-
+    numKeys = 0;
+    numBuckets = DEFAULT_BUCKETS;
+    maxLoad = DEFAULT_LOAD;
 }
 
 template<class K, class T>
 void MapT<K, T>::Add(K key, T value) {
 
+    Remove(key);
+
     // Find the appropriate bucket that key lives in
     int bucket = GetHashIndex(key);
 
+    /*check to see if the key already exists, if it does change the value
+     *associated with the key
+    for (auto it = buckets[bucket].begin(); it != buckets[bucket].end(); ++it)
+    {
+        if (it->first == key) // does the key exist?
+        {
+            it->second = value;
+            return;
+        }
+    }*/
 
+    pair<K,T> keyVal;
+    keyVal.first = key;
+    keyVal.second = value;
+
+    buckets[bucket].push_front(keyVal);
+    numKeys++;
+
+    if (LoadFactor() > maxLoad)
+    {
+        Rehash(2*numBuckets);
+    }
 }
 
 template<class K, class T>
@@ -29,6 +55,15 @@ void MapT<K, T>::Remove(K key) {
     // Find the appropriate bucket that key lives in
     int bucket = GetHashIndex(key);
 
+    for (auto it = buckets[bucket].begin(); it != buckets[bucket].end(); ++it)
+    {
+        if (it->first == key) // does the key exist?
+        {
+            buckets[bucket].remove(*it);
+            numKeys--;
+            return;
+        }
+    }
 
 }
 
@@ -37,7 +72,13 @@ bool MapT<K, T>::Contains(K key) {
     // Find the appropriate bucket that key lives in
     int bucket = GetHashIndex(key);
 
-
+    for (auto it = buckets[bucket].begin(); it != buckets[bucket].end(); ++it)
+    {
+        if (it->first == key) // does the key exist?
+        {
+            return true;
+        }
+    }
     return false;
 }
 
@@ -45,9 +86,37 @@ template<class K, class T>
 T MapT<K, T>::operator[](K key) {
     int bucket = GetHashIndex(key);
 
-
+    for (auto it = buckets[bucket].begin(); it != buckets[bucket].end(); ++it)
+    {
+        if (it->first == key) // does the key exist?
+        {
+            return it->second;
+        }
+    }
 
     throw KeyDoesNotExist();
+}
+
+template <class K, class T>
+MapT<K, T>& MapT<K, T>::operator=(MapT const& other)
+{
+    delete [] buckets; //clear out "this" buckets
+
+    numBuckets = other.numBuckets;
+    buckets = new forward_list<pair<K,T>>[numBuckets];
+    numKeys = 0;
+
+    //loop thru all values in other
+    for (int b = 0; b < other.numBuckets; b++)
+    {
+        //loop thru all values in bucket b
+        for (auto it = other.buckets[b].begin(); it != other.buckets[b].end(); ++it)
+        {
+            Add(it->first, it->second);
+        }
+    }
+
+    return *this;
 }
 
 template<class K, class T>
@@ -64,6 +133,17 @@ template<class K, class T>
 void MapT<K, T>::Rehash(int numBuckets) {
     MapT<K, T> newMap(numBuckets);  // Need to copy over all elements to newMap
 
+    //loop thru all values in other
+    for (int b = 0; b < numBuckets; b++)
+    {
+        //loop thru all values in bucket b
+        for (auto it = buckets[b].begin(); it != buckets[b].end(); ++it)
+        {
+            newMap.Add(it->first, it->second);
+        }
+    }
+
+    *this = newMap;
 }
 
 template<class K, class T>
